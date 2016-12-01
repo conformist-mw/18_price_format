58e0892705e860cccd69c0cada31d32dff68fd19
conformist-mw <conformist.mw@gmail.com>
2016-12-01 22:44:57 +0200

    implemented price format functionality

--- format_price.py
+++ format_price.py
@@ -1,5 +1,16 @@
+import re
+match = re.compile(r'\d+\.?\d*')
+
+
 def format_price(price):
-    pass
+    if (isinstance(price, (int, float)) or
+            isinstance(price, str) and re.match(match, price)):
+        price = float(price)
+        if price.is_integer():
+            return format(price, ',.0f').replace(',', ' ')
+        else:
+            return format(price, ',.2f').replace(',', ' ')
+
 
 if __name__ == '__main__':
     pass
